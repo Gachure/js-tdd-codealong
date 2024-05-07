1199ba80d1c0c88b506e80a5008f167046121578
victor <victor.gachure@student.moringaschool>
2024-05-07 17:04:10 +0300

    done lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,17 @@
+// src/__tests__/utils.test.js
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+    // Arrange
     const birthYear = 1984;
-    const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    const currentYear = new Date().getFullYear();
+    const expectedAge = currentYear - birthYear;
+
+    // Act
+    const age = currentAgeForBirthYear(birthYear);
+
+    // Assert
+    expect(age).toBe(expectedAge);
   });
 });
